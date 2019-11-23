634c95530f3273358bd34c4ea2a19e1dffcea72e
Mansur Beştaş <mansur@siirt.edu.tr>
2019-11-24 00:04:42 +0300

    Update ayar.php

--- ayar.php
+++ ayar.php
@@ -1,8 +1,8 @@
 <?php
 // veritabanin ayarlarini yapiyoruz.
-$vt_kullanici="ylbasvuru";
-$vt_parola="2a4y3a7y3";
-$vt_isim="zadmin_basvuru";
+$vt_kullanici="";
+$vt_parola="";
+$vt_isim="";
 $vt_sunucu="localhost";
  
 // ezSQL sinifini cagirarak calistirmaya basliyoruz.
